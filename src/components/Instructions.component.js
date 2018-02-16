59fc293d0d3d43efc6bbd8aa37a811d33c415950
Prashant S Pokhriyal <psp@wirelessprv-10-192-237-25.near.illinois.edu>
2018-02-16 16:14:07 -0600

    Part 1 Done

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -5,16 +5,25 @@ import './../styles/instructions.css'
 
 class Instructions extends Component {
   render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
-      </div>
-    )
+    let shouldDisplayImage = this.props.shouldDisplayImage
+    if (shouldDisplayImage) {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+        </div>
+      )
+    }
   }
 }
 
