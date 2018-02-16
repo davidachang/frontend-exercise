07c660d0804f5de3d49c0f23bbfc66a76a7b69e7
Deanplayerljx <deanplayerljx@gmail.com>
2018-02-16 14:49:07 -0600

    part1

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -4,15 +4,20 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div className="instructions">
         Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        {this.props.shouldDisplayImage && (
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        )}
       </div>
     )
   }
