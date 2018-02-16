d9757605e565b571dfe5cd4df321bb6874baa399
Feeshay <michaelchen578@gmail.com>
2018-02-16 16:12:00 -0600

    Part 1

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 // TODO: uncomment this for Part 1
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
   render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
-      </div>
-    )
+    let shouldDisplayImage = this.props.shouldDisplayImage
+    //let to declare variable
+    //this.props to access (sim instance fields)
+
+    if (shouldDisplayImage) {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+        </div>
+      )
+    }
   }
 }
 
