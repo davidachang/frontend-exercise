45b239a4b884edc20c5691b1f045c277928aa8bf
shreyshrey1 <talk2shreyas@gmail.com>
2018-02-15 21:50:30 -0600

    Part 1

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -3,30 +3,17 @@ import PropTypes from 'prop-types'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
-    if (this.props.shouldDisplayImage) {
-      return (
-        <div className="instructions">
-          Follow the instructions on the README to get started!
+    return (
+      <div className="instructions">
+        Follow the instructions on the README to get started
+        {this.props.shouldDisplayImage && (
           <img
             className="instructions__logo"
             src="https://uiuc.hack4impact.org/img/colored-logo.png"
             alt="h4i logo"
           />
-        </div>
-      )
-    }
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        )}
       </div>
     )
   }
@@ -34,9 +21,6 @@ class Instructions extends Component {
 
 Instructions.propTypes = {
   shouldDisplayImage: PropTypes.bool
-}
-Instructions.defaultProps = {
-  shouldDisplayImage: false
 }
 
 export default Instructions
