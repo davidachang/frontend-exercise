18d7d7bd349f3b23790143143a8543a080b28499
anoojlal <anoojl2@illinois.edu>
2018-02-17 16:26:20 -0600

    Part 3 Complete

---
+++ src/components/Counter.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './../styles/instructions.css'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+
+    this.handleIncrement = this.handleIncrement.bind(this)
+    this.handleDecrement = this.handleDecrement.bind(this)
+  }
+
+  handleIncrement() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleDecrement() {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        <label>{this.state.count}</label>
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter
