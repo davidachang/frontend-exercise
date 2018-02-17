6cf8e727179719b9d6e16120b6c6824b4aa37347
Pavani Malli <pavanim@wirelessprv-10-195-39-167.near.illinois.edu>
2018-02-17 17:08:01 -0600

    Part 4 Complete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 import { Instructions } from './components'
 import { Counter } from './components'
 import './styles/app.css'
+import PropTypes from 'prop-types'
 
 class App extends Component {
+  state = { initialCount: 0, shouldUpdate: false }
+
+  handleSubmitClick = event => {
+    this.setState({ initialCount: event.target.value })
+  }
+
+  handleChange = event => {
+    event.preventDefault()
+    this.setState({ shouldUpdate: true })
+  }
+
   render() {
     var items = [
       { string: 'f1' },
@@ -13,6 +25,8 @@ class App extends Component {
       { string: 'fifth bullet' }
     ]
 
+    const { count } = this.state.initialCount
+
     return (
       <div className="app">
         <h1>Hello World!</h1>
@@ -20,10 +34,21 @@ class App extends Component {
           shouldDisplayName={true}
           items={items.filter(item => item.string.length >= 3)}
         />
-        <Counter />
+        <input
+          type="number"
+          name="Initial Value: "
+          value={this.state.initialCount}
+          onChange={this.handleSubmitClick}
+        />
+        <button onClick={this.handleChange}> Submit </button>
+        {this.state.shouldUpdate && <Counter count={this.state.initialCount} />}
       </div>
     )
   }
+}
+
+App.propTypes = {
+  initialCount: PropTypes.number
 }
 
 export default App
