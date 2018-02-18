aad6be326b4828d8c3836222243065e0e9a341ef
alan-fang <fang.alan.7@gmail.com>
2018-02-18 02:38:21 -0600

    Part 1 Complete

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -5,16 +5,20 @@ import './../styles/instructions.css'
 
 class Instructions extends Component {
   render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
-      </div>
-    )
+    const display = this.props.shouldDisplayImage
+    if (display) {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        </div>
+      )
+    }
+    return null
   }
 }
 
