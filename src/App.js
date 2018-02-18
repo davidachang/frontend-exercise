77283226828d3d01b6eff567cdc2f98c713b88ee
shreyshrey1 <talk2shreyas@gmail.com>
2018-02-18 13:48:31 -0600

    Part 3 doesn't work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Instructions } from './components'
+import { Counter } from './components'
 import './styles/app.css'
 
 class App extends Component {
@@ -7,8 +8,11 @@ class App extends Component {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <Instructions shouldDisplayImage={true} />
-        <Instructions list={['hello', 'my', 'name', 'is', 'shreyas']} />
+        <Instructions
+          shouldDisplayImage={true}
+          list={['hello', 'my', 'name', 'is', 'shreyas']}
+        />
+        <Counter iniitialCount={0} />
       </div>
     )
   }
---
+++ src/components/Counter.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './../styles/instructions.css'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: this.props.initialCount }
+  }
+  increment = () => {
+    this.setState(state => ({
+      count: state.count + 1
+    }))
+  }
+  decrement = () => {
+    this.setState(state => ({
+      count: state.count - 1
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.decrement}>-</button>
+        Count state: {this.state.count}
+        <button onClick={this.increment}>+</button>
+      </div>
+    )
+  }
+}
+
+Counter.propTypes = {
+  initialCount: PropTypes.number.isRequired
+}
+
+export default Counter
