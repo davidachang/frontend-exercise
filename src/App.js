70c9fcd22385f1bd72cb963ae7ff11c37cdbc5e9
Skyler Shi <jingtao2@illinois.edu>
2018-02-18 10:40:51 -0600

    Part 1 Complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import { Instructions } from './components'
 import './styles/app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shouldDisplayImage: true
+    }
+  }
   render() {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <Instructions />
+        <Instructions shouldDisplayImage={this.state.shouldDisplayImage} />
       </div>
     )
   }
