2c2a1bf4d991627ef7711b0f49f770b1f30d9282
Alan Ren <alanren2@illinois.edu>
2018-02-20 22:25:45 -0600

    Part 1 Complete

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// TODO: uncomment this for Part 1
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
@@ -8,11 +7,13 @@ class Instructions extends Component {
     return (
       <div className="instructions">
         Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        {this.props.shouldDisplayImage && (
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        )}
       </div>
     )
   }
