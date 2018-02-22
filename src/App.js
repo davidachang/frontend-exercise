e3dde1f6ce9fcf0042df34818b906c3da449081f
Alan Ren <alanren2@illinois.edu>
2018-02-21 21:24:50 -0600

    Part 3 Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 import { Instructions } from './components'
+// import Counter from './components'
 import './styles/app.css'
+
+class Counter extends Component {
+  state = { count: 0 }
+  handleUp = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleDown = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        Count:
+        <data> {count} </data>
+        <button onClick={this.handleUp}> Increment </button>
+        <button onClick={this.handleDown}> Decrement </button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -11,6 +33,7 @@ class App extends Component {
           shouldDisplayImage={true}
           items={['apple', 'banana', 'c']}
         />
+        <Counter />
       </div>
     )
   }
